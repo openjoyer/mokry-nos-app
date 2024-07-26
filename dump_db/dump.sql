5caafc90aa3b06726aa9085d75e5cb0249c79a23
tylerpants <burlakaegor777@gmail.com>
2024-07-26 10:54:41 +0700

    add Liquibase migration, change dump.sql

--- dump_db/dump.sql
+++ dump_db/dump.sql
@@ -1,86 +1,27 @@
--- MySQL dump 10.13  Distrib 8.0.37, for Linux (x86_64)
---
--- Host: localhost    Database: mokriy_nos_app_db
--- ------------------------------------------------------
--- Server version	8.0.37-0ubuntu0.24.04.1
+CREATE DATABASE `mokriy-nos-app-db`;
+CREATE TABLE `mokriy-nos-app-db`.messages (
+                            id int NOT NULL AUTO_INCREMENT,
+                            title varchar(50) NOT NULL,
+                            type int NOT NULL,
+                            text varchar(1000) NOT NULL,
+                            created_date timestamp NULL DEFAULT NULL,
+                            person_name varchar(100) NOT NULL,
+                            is_marked tinyint(1) DEFAULT NULL,
+                            PRIMARY KEY (id),
+                            UNIQUE KEY id (id),
+                            UNIQUE KEY title (title)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+INSERT INTO `mokriy-nos-app-db`.messages VALUES (1,'Пример сообщения',2,'При ненадобности удалить','2024-07-24 17:08:16','Иванов Иван',0);
 
---
--- Table structure for table `messages`
---
+CREATE TABLE `mokriy-nos-app-db`.users (
+                         id int NOT NULL AUTO_INCREMENT,
+                         username varchar(50) NOT NULL,
+                         password varchar(100) NOT NULL,
+                         role varchar(20) NOT NULL,
+                         PRIMARY KEY (id),
+                         UNIQUE KEY id (id),
+                         UNIQUE KEY username (username)
+);
 
-DROP TABLE IF EXISTS `messages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `messages` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) NOT NULL,
-  `type` int NOT NULL,
-  `text` varchar(1000) NOT NULL,
-  `created_date` timestamp NULL DEFAULT NULL,
-  `person_name` varchar(100) NOT NULL,
-  `is_marked` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `title` (`title`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `messages`
---
-
-LOCK TABLES `messages` WRITE;
-/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` VALUES (1,'Пример сообщения',2,'При ненадобности удалить','2024-07-24 17:08:16','Иванов Иван',0);
-/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `role` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','$2a$10$AGQ7eTmySwy1cvAPdVC5k.B9kCe3cytctQ1TgpiGfPLUM2CKcGYVO','ROLE_ADMIN');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-07-25 15:46:09
+INSERT INTO `mokriy-nos-app-db`.users VALUES (1,'admin','$2a$10$AGQ7eTmySwy1cvAPdVC5k.B9kCe3cytctQ1TgpiGfPLUM2CKcGYVO','ROLE_ADMIN');
